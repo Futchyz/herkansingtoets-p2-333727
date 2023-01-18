5e42dc41abbf09b69f575e9bcfb0ad04f3e8ee8b
Imran <333727@student.mboutrecht.nl>
2023-01-18 12:10:15 +0100

    6

--- app/views/Instructeurs/index.php
+++ app/views/Instructeurs/index.php
@@ -13,4 +13,4 @@
         <?= $data['rows']; ?>
     </tbody>
 </table>
-<p><a href="<?= URLROOT; ?>/landingpages/index">Back To Landingpage</a></p>
+<p><a href="<?= URLROOT; ?>/landingpages/index">back To Landingpage</a></p>
