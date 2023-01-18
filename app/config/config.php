7641154b66c34b6d50c2638c7a21638b818d86b2
Imran <333727@student.mboutrecht.nl>
2023-01-18 11:20:21 +0100

    1

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'herkansingstoets');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://mvc-oop-pdo-herkansing-userstory.com');
+
+define('SITENAME', 'herkansing_userstory');
---
+++ app/views/landingpages/index.php
@@ -0,0 +1,10 @@
+<h3><?= $data['title']; ?></h3>
+<p><?= $data['sayHello']; ?></p>
+<ul>
+    <li>
+        <a href="<?= URLROOT; ?>/Instructeurs/index">Alle instructeurs zonder auto</a>        
+    </li>
+    <li>
+        <a href="<?= URLROOT; ?>/lessen/index">Lesrooster weergeven</a>
+    </li>
+</ul>
